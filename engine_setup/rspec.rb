4d7af96e798e249e636c0a3ac78b6bc0e4d8d744
alexisKinto <mgutierrez@afal.mx>
2022-05-25 17:11:16 -0700

    upload rspec and rubocop templates

---
+++ engine_setup/rspec.rb
@@ -0,0 +1,145 @@
+# frozen_string_literal: true
+
+# --------------------------------------------------
+#             RSpec github action setup
+# --------------------------------------------------
+def create_github_workflows_directory
+  run 'mkdir .github' unless Dir.exist?('.github')
+  run 'mkdir .github/workflows' unless Dir.exist?('.github/workflows')
+end
+
+def create_rspec_github_action
+  run 'touch .github/workflows/rspec.yml' unless File.exist?('.github/workflows/rspec.yml')
+  append_file "#{Dir.pwd}/.github/workflows/rspec.yml", <<~'YML'
+    name: RSpec Tests
+    on:
+      push:
+        branches: [main]
+      pull_request:
+        branches: [main]
+      workflow_call:
+        secrets:
+          CC_TEST_REPORTER_ID:
+            description: 'Code Climate Reporter ID for current repo'
+            required: false
+    jobs:
+      test:
+        runs-on: ubuntu-latest
+        steps:
+          - uses: amancevice/setup-code-climate@v0
+            name: CodeClimate Install
+            with:
+              cc_test_reporter_id: ${{ secrets.CC_TEST_REPORTER_ID }}
+          - name: Checkout code
+            uses: actions/checkout@v3
+          # Add or replace dependency steps here
+          - name: Install Ruby and gems
+            uses: ruby/setup-ruby@v1
+            with:
+              ruby-version: 3.1.2
+              bundler-cache: true
+          - name: CodeClimate Pre-build Notification
+            run: cc-test-reporter before-build
+          # Add or replace test runners here
+          - name: Run RSpec
+            run: bundle exec rspec spec/
+          - name: CodeClimate Post-build Notification
+            run: cc-test-reporter after-build -t simplecov -r ${{secrets.CC_TEST_REPORTER_ID}}
+  YML
+end
+
+def add_simplecov
+  append_file "#{Dir.pwd}/Gemfile", <<~'TEXT'
+    gem 'simplecov', group: 'test'
+  TEXT
+
+  run 'bundle install'
+
+  prepend_file "#{Dir.pwd}/spec/rails_helper.rb", <<~'RUBY'
+    require 'simplecov'
+
+    SimpleCov.start 'rails' do
+      add_filter '/spec/'
+
+      add_filter(/preview.rb/)
+      add_filter(/stories.rb/)
+    end
+  RUBY
+end
+
+def git_ignore_rspec_coverage
+  append_file "#{Dir.pwd}/.gitignore", <<~'TEXT'
+    /coverage/
+  TEXT
+end
+
+# --------------------------------------------------
+#                 RSpec rails setup
+# --------------------------------------------------
+
+# rubocop:disable Metrics/MethodLength
+def create_dummy_app
+  # Create temporary engine to build the dummy app,
+  # copy the dummy/spec/ directory to your current engine's root path
+  # and remove the dummy engine
+  run 'rails plugin new dummy --full --dummy-path=spec/dummy -T'
+  run 'cp -r dummy/spec/. spec'
+  run 'rm -rf dummy'
+
+  append_file "#{Dir.pwd}/.gitignore", <<~'TEXT'
+    /spec/dummy/db/*.sqlite3
+    /spec/dummy/db/*.sqlite3-*
+    /spec/dummy/log/*.log
+    /spec/dummy/node_modules/
+    /spec/dummy/storage/
+    /spec/dummy/tmp/
+    /spec/dummy/tmp/development_secret.txt
+  TEXT
+
+  gsub_file "#{Dir.pwd}/bin/rails", '../test/dummy/config/application', '../spec/dummy/config/application'
+  gsub_file "#{Dir.pwd}/spec/dummy/config/application.rb", /require "dummy"/, "# require 'dummy'"
+  gsub_file "#{Dir.pwd}/Rakefile", 'test/dummy/Rakefile', 'spec/dummy/Rakefile'
+end
+# rubocop:enable Metrics/MethodLength
+
+def remove_test_dir
+  run 'rm -rf test' if Dir.exist?('test')
+  gsub_file "#{Dir.pwd}/.gitignore", %r{/test/dummy/.*}, ''
+end
+
+def add_dependencies_to_gemfile(engine_name)
+  inject_into_file "#{Dir.pwd}/#{engine_name}.gemspec", after: "Gem::Specification.new do |spec|\n" do
+    <<~'RUBY'
+      spec.add_development_dependency 'rspec-rails'
+    RUBY
+  end
+end
+
+def add_rspec_rails_gem
+  append_file "#{Dir.pwd}/Gemfile", <<~'TEXT'
+    gem 'rspec-rails', '~> 5', group: [:development, :test]
+  TEXT
+
+  run 'bundle install'
+end
+
+# --------------------------------------------------
+#                       MAIN
+# --------------------------------------------------
+puts "\n\nStarting RSpec setup..\n\n"
+
+name = ask("What's yout engine name? (You can check your engine name on the .gemspec file):")
+
+create_dummy_app
+remove_test_dir
+add_dependencies_to_gemfile(name)
+add_rspec_rails_gem
+rails_command 'generate rspec:install'
+
+puts('Creating Rspec Github Action...')
+create_github_workflows_directory
+create_rspec_github_action
+add_simplecov
+git_ignore_rspec_coverage
+
+puts "\n\nRSpec setup finished!\n\n"
---
+++ engine_setup/rubocop.rb
@@ -0,0 +1,150 @@
+# frozen_string_literal: true
+
+def create_rubocop_yml_configuration
+  run 'touch .rubocop.yml'
+
+  append_file "#{Dir.pwd}/.rubocop.yml", <<~'YML'
+    require: rubocop-rails
+
+    AllCops:
+      TargetRubyVersion: 3.1.2
+      NewCops: enable
+      SuggestExtensions: false
+      Exclude:
+        - 'coverage/**/*'
+        - 'log/*'
+        - 'config/**/*'
+        - 'public/**/*'
+        - 'storage/**/*'
+        - 'tmp/**/*'
+        - 'script/**/*'
+        - 'node_modules/**/*'
+        - 'lib/tasks/bali_tasks.rake'
+        - 'bin/**/*'
+        - 'vendor/**/*'
+        - 'spec/spec_helper.rb'
+        - 'spec/dummy/bin/*'
+        - 'spec/dummy/config/**/*'
+        - 'spec/dummy/db/**/*'
+        - !ruby/regexp /old_and_unused\.rb$/
+
+    Gemspec/RequiredRubyVersion:
+      Enabled: false
+
+    Layout/LineLength:
+      Max: 100
+
+    Layout/SpaceBeforeBrackets:
+      Enabled: false
+
+    Metrics/ClassLength:
+      Max: 150
+
+    Metrics/CyclomaticComplexity:
+      Max: 8
+
+    Metrics/MethodLength:
+      Max: 30
+
+    Metrics/ParameterLists:
+      Max: 6
+
+    Metrics/PerceivedComplexity:
+      Max: 10
+
+    Style/Documentation:
+      Enabled: false
+
+    Style/HashEachMethods:
+      Enabled: true
+
+    Style/HashTransformKeys:
+      Enabled: true
+
+    Style/HashTransformValues:
+      Enabled: true
+
+    Style/Lambda:
+      Enabled: false
+
+    Rails:
+      Enabled: true
+
+    Rails/OutputSafety:
+      Enabled: false
+
+    Rails/SkipsModelValidations:
+      Enabled: false
+
+    Rails/HasAndBelongsToMany:
+      Enabled: false
+
+    Metrics/BlockLength:
+      IgnoredMethods: ['describe', 'context', 'it', 'before', 'included']
+      Exclude:
+        - spec/*
+        - lib/tasks/*
+
+    Naming/VariableNumber:
+      Enabled: false
+
+    Naming/RescuedExceptionsVariableName:
+      Enabled: false
+
+    Lint/EmptyBlock:
+      Exclude:
+        - spec/**/*
+
+    Lint/MissingSuper:
+      Exclude:
+        - app/components/**/*
+
+    Style/RedundantBegin:
+      Enabled: false
+  YML
+end
+
+def add_rubocop_gems
+  append_file "#{Dir.pwd}/Gemfile", <<~'TEXT'
+    gem 'rubocop', '~> 1', require: false , group: :development
+    gem 'rubocop-rails', '~> 2', group: :development
+  TEXT
+
+  run 'bundle install'
+end
+
+def create_rubocop_github_action
+  run 'touch .github/workflows/rubocop.yml'
+
+  append_file "#{Dir.pwd}/.github/workflows/rubocop.yml", <<~'YML'
+    name: Rubocop
+    on: push
+
+    jobs:
+      rubocop:
+        name: Rubocop
+        runs-on: ubuntu-latest
+
+        steps:
+          - uses: actions/checkout@v3
+          - uses: ruby/setup-ruby@v1
+            with:
+              ruby-version: 3.1.2
+              bundler-cache: true
+          - run: bundle install
+          - name: Rubocop
+            run: bundle exec rubocop
+  YML
+end
+
+# --------------------------------------------------
+#                       MAIN
+# --------------------------------------------------
+puts "\n\nStarting Rubocop setup..\n\n"
+add_rubocop_gems
+create_rubocop_yml_configuration
+
+puts('Creating Rubocop Github Action...')
+create_rubocop_github_action
+
+puts "\n\nRubocop setup finished!\n\n"
