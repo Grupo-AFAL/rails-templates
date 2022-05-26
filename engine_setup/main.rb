4afdd530d217eb932465e741ae7727f34cb3e239
alexisKinto <mgutierrez@afal.mx>
2022-05-25 17:32:25 -0700

    main engine setup template

---
+++ engine_setup/main.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+def run_template(file_name)
+  location = "https://raw.githubusercontent.com/Grupo-AFAL/rails-templates/main/engine_setup/#{file_name}"
+  run "rails app:app:template LOCATION='#{location}'"
+end
+
+puts "\n\n💎 Welcome to the Rails engine setup! 💎\n\n"
+
+# Required
+run_template('rspec.rb')
+run_template('rubocop.rb')
